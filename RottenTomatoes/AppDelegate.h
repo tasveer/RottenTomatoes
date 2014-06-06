ebc4540610470985b1f8787b6bdf6a247fa26a95
Hunaid <hunaid@hotmail.com>
2014-06-05 22:29:40 -0700

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Hunaid Hussain on 6/5/14.
+//  Copyright (c) 2014 kolekse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
